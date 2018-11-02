b56009138b617b9615660d45691a75c7c425ba7f
FHGDev <gideon.lingle1@gmail.com>
2018-11-01 17:15:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("You are using dbl-api v1.0.0 by FHGDev.")
 const api = new (require('./src/api.js'))()
 const options = {
-  token: process.env.token,
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI5NDE5NDUwNjExMzIyMDYwOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQwODI5Mjc4fQ.agZMsjRNQLU4KRNv9iLGMMSfIkPfKfU5Z3SV4zGP4Pg",
   servercount: 500,
   botid: 294194506113220608
 }
