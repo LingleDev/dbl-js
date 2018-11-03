b0cda47d4724554e782c523ed578544b098983a1
FHGDev <gideon.lingle1@gmail.com>
2018-11-02 19:29:09 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ class DBL {
       throw new RangeError("Missing Parameters.");
     }
     
-    fetch.post(`https://discordbots.org/api/bots/stats`)
+    fetch.post(`https://discordbots.org/api/bots/${botid}/stats`)
     .set("Authorization", options.token)
     .send({count: options.servercount})
     .then(r => {
